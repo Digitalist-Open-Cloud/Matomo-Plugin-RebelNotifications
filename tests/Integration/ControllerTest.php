bf99860d5ef51b64182d82bdb20479b4dbe19602
Mikke Schirén <mikke.schiren@digitalist.com>
2026-03-01 15:13:05 +0100

    fix test

--- tests/Integration/ControllerTest.php
+++ tests/Integration/ControllerTest.php
@@ -80,7 +80,7 @@ class ControllerTest extends IntegrationTestCase
         $this->api->insertNotification('1', 'Test notification', 'bar', 'warning', '25', 'persistent', '0');
         $result = $this->controller->index();
         $this->assertIsString($result);
-        $this->assertStringContainsString('action="{{ linkTo({\'module\':\'RebelNotifications\',\'action\':\'deleteNotification\'}) }}"', $result);
+        $this->assertStringContainsString('RebelNotifications_DeleteNotification', $result);
     }
 
     public function testEditNotificationGeneratesUpdateNonce()
